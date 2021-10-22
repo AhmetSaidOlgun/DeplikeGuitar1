548cd2d47c81f7176c4a61a9db835f34e7135c9d
ageratinaaltissima <said.olgun@boun.edu.tr>
2021-10-22 11:52:58 +0300

    Initial commit

---
+++ Assets/Scripts/Cam2Rotate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam2Rotate : MonoBehaviour
+{
+    private float x;
+    private float y;
+    private Vector3 rotateValue;
+    
+    private GameObject GameControl;
+    private GameController ControlScript;
+
+    private void Start()
+    {
+        GameControl = GameObject.FindGameObjectWithTag("GameController");
+        ControlScript = GameControl.GetComponent<GameController>();
+    }
+ 
+    void Update()
+    {
+        y = Input.GetAxis("Mouse X");
+        //x = Input.GetAxis("Mouse Y");
+        Debug.Log(x + ":" + y);
+        rotateValue = new Vector3(x, y * -1, 0);
+        transform.eulerAngles = transform.eulerAngles - rotateValue;
+
+        if (y > 0.2 || y<-0.2)
+        {
+            
+            if (ControlScript.t7 != null && ControlScript.t8 != null)
+            {
+                Destroy(ControlScript.t7);
+                ControlScript.t8.SetActive(true);
+            }
+           
+        }
+        
+        
+    }
+}
